43f50217b4932cbcfc057e934af4f714caa17c75
Leonid Dashko <all-cs.info@i.ua>
2018-02-17 14:15:00 +0200

    Refactor requests to SEB API

--- seb_api.py
+++ seb_api.py
@@ -1,10 +1,15 @@
 import requests
 import json
+from flask_login import current_user
 
 
-class SebApi(object):
+class SebApi:
+    API_URL = 'https://test.api.ob.baltics.sebgroup.com/v1/bics'
 
-    def __init__(self, ttp_token, user_token):
+    def __init__(self):
+        ttp_token = """eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsdmY0MzJAbWFpbC5ydSIsImV4cCI6MTU1MDM5NDg0M30.PNC0Igse8izoiDhfqiHqEi3MQ_RXmsJO3rlHdlBrershU_WT1YRhVq8juaSygplQVspX59A6JduFJnW3skZmjw"""
+        user_token = """eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJERU1PRUUsaWJzVXNlcjEiLCJleHAiOjE1NTAzOTQ3MTl9.uunOsh99Zp-lcOAkN6XpJ_q4h0JJ_XVaak3I-n_z6yXbw_bND1eb4kWm0WiF96Vnkr3NeEzC7DR62W3J3WgNFg"""
+
         self.ttp_token = ttp_token
         self.user_token = user_token
 
@@ -13,72 +18,19 @@ class SebApi(object):
             'content-type': "*/*",
             'Tpp-Token': ttp_token,
             'User-Token': user_token
-            }
+        }
 
-    # def get_payments(self, start, end):
-    #
-    #     payload = {
-    #         "from": start,
-    #         "to": end,
-    #
-    #     }
-    #
-    #     request_url = """https://test.api.ob.baltics.sebgroup.com/v1/bics/EEUHEE2X/eris/payments?"""
-    #
-    #     r = requests.get(request_url, headers=self.headers, params=payload, verify=False)
-    #     events = r.json()["events"]
-    #     payment_ids = [event.get("paymentReferenceID") for event in events]
-    #     return payment_ids
-    #
-    # def show_payments(self, payments_ids):
-    #
-    #     request_url = """https://test.api.ob.baltics.sebgroup.com/v1/payments/sepa-credit-transfers/initiation"""
-    #
-    #     payments_data = {}
-    #
-    #     for payment_id in payments_ids:
-    #         payload = {
-    #             "paymentId": payment_id
-    #         }
-    #
-    #         r = requests.get(request_url, headers=self.headers, params=payload, verify=False)
-    #         print(r.json())
-    #         creation_time = r.json()["creationDateTime"]
-    #         amount = r.json()["amount"]
-    #         currency = r.json()["currency"]
-    #         creditor = r.json()["creditor"]
-    #         info = r.json()["remittanceInformationUnstructured"]
-    #
-    #         payments_data[payment_id] = [creation_time, amount, currency, creditor, info]
-    #
-    #     return payments_data
+    def get_payments(self, size=10, date_from="2017-12-01", date_to="2018-03-01", page=0):
+        endpoint = self.API_URL + '/EEUHEE2X/accounts/eRnQSm4QiGMkHhr2UaStGizMjTr-BO9FkDkv8SuWc5E/transactions?'
+        payload = {
+            "from": date_from,
+            "to": date_to,
+            "page": page,
+            "size": size
+        }
+        resp = requests.get(endpoint, headers=self.headers, params=payload, verify=False)
+        payment_info = resp.json()["content"]
+        return payment_info
 
-    def get_payments(self, n):
-            payment_info = {}
-
-            payload = {
-                "from": "2017-12-01",
-                "to": "2018-03-01",
-                "page": 0,
-                "size": n
-            }
-
-            request_url = """https://test.api.ob.baltics.sebgroup.com/v1/bics/EEUHEE2X/accounts/eRnQSm4QiGMkHhr2UaStGizMjTr-BO9FkDkv8SuWc5E/transactions?"""
-
-            r = requests.get(request_url, headers=self.headers, params=payload, verify=False)
-
-            for index, payment in enumerate(r.json()["content"]):
-                amount = payment["transactionAmount"]
-                currency = payment["transactionCurrency"]
-                date = payment["bookdate"]
-                party_name = payment["counterPartyName"]
-                payment_info[index] = [amount, currency, date, party_name]
-
-            return payment_info
-
-TTP = """eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsdmY0MzJAbWFpbC5ydSIsImV4cCI6MTU1MDM5NDg0M30.PNC0Igse8izoiDhfqiHqEi3MQ_RXmsJO3rlHdlBrershU_WT1YRhVq8juaSygplQVspX59A6JduFJnW3skZmjw"""
-USER = """eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJERU1PRUUsaWJzVXNlcjEiLCJleHAiOjE1NTAzOTQ3MTl9.uunOsh99Zp-lcOAkN6XpJ_q4h0JJ_XVaak3I-n_z6yXbw_bND1eb4kWm0WiF96Vnkr3NeEzC7DR62W3J3WgNFg"""
-
-seb = SebApi(TTP, USER)
-print(seb.get_payments(10))
-
+# seb = SebApi()
+# print(seb.get_payments(10))
